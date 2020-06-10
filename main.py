e043781e34bc9cf68778574f9d8aef0a17337405
boite2chocolat <samylacombe@gmail.com>
2020-06-11 00:23:46 +0200

    2nd commit

--- main.py
+++ main.py
@@ -55,17 +55,42 @@ class Player(Ship):
 		self.mask = pygame.mask.from_surface(self.ship_img)
 		self.max_health = health
 
+
+class Enemy(Ship):
+	COLOR_MAP = {
+				"red": (RED_SHIP,RED_LASER),
+				"green": (GREEN_SHIP,GREEN_LASER),
+				"blue": (BLUE_SHIP,BLUE_LASER)
+				}
+
+	def __init__(self,x,y,color, health=100):
+		super().__init__(x, y, health)
+		self.ship_img, self.laser_img = self.COLOR_MAP[color]
+		self.mask = pygame.mask.from_surface(self.ship_img)
+
+	def move(self, vel):
+		self.y += vel
+
 def main():
 	run = True 
 	FPS = 60   # image par seconde
-	level = 1
+	level = 0
 	lives = 5
 	main_font = pygame.font.SysFont("comicsans", 50)
+	lost_font = pygame.font.SysFont("comicsans", 60)
+
+	enemies = []
+	wave_length = 5 
+	enemy_vel = 1
 	player_vel = 5
+
 
 	player = Player(300, 650)
 
 	clock  = pygame.time.Clock()
+
+	lost = False
+	lost_count = 0
 
 	def redraw_window():
 		WIN.blit(MAP, (0,0))
@@ -76,13 +101,39 @@ def main():
 		WIN.blit(lives_label, (10,10))
 		WIN.blit(level_label, (WIDTH - level_label.get_width() - 10, 10))
 
+		for enemy in enemies:
+			enemy.draw(WIN)
+
 		player.draw(WIN)
+
+		if lost:
+			lost_label = lost_font.render("Game Over !",1, (255,255,255))
+			WIN.blit(lost_label, (WIDTH/2 - lost_label.get_width()/2, 350))
 
 		pygame.display.update()
 
 	while run:
 		clock.tick(FPS)
 		redraw_window()
+		if lives <= 0 or player.health <= 0:
+			lost = True
+			lost_count +=1
+
+		if lost:
+			if lost_count > FPS * 3:
+				run = False
+			else:
+				continue
+
+		if len(enemies) == 0:
+			level += 1 
+			wave_length += 5
+
+			for i in range(wave_length):
+				enemy = Enemy(random.randrange(50, WIDTH-100), random.randrange(-1500, -100), random.choice(["red","blue","green"]))
+				enemies.append(enemy)
+
+
 		for event in pygame.event.get():
 			if event.type == pygame.QUIT:
 				run = False
@@ -96,5 +147,13 @@ def main():
 			player.y -= player_vel
 		if keys[pygame.K_DOWN] and player.y + player_vel + player.get_height() < HEIGHT: # Deplacement vers le bas 
 			player.y += player_vel
+
+		for enemy in enemies[:]:
+			enemy.move(enemy_vel)
+			if enemy.y + enemy.get_height() > HEIGHT:
+				lives -= 1
+				enemies.remove(enemy)
+
+		
 
 main()
